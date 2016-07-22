6e64b875509319751594bbad66fca98ba822f515
ZYH <sqhtiamo@163.com>
2016-07-22 13:54:32 +0800

    add babel

---
+++ src/index.js
@@ -0,0 +1,26 @@
+class ImageComposer {
+
+	constructor (...args) {
+
+	}
+
+	compose () {
+
+
+	}
+
+	valueOf () {
+
+		return this;
+
+	}
+
+	toString () {
+
+		return this;
+
+	}
+}
+
+module.exports = ImageComposer;
+
