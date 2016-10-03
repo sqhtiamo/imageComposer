f4a19f9a2305e59515c6d043a7027abffd77f4ee
ZYH <sqhtiamo@163.com>
2016-10-03 09:25:43 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Image Coposer
+
+Composing Mutilple Image to One Image with node-canvas.
+
+## Installation
+
+npm install
+
+## Exmaple
+
+node example/index.js
+
+## Output
+
+![Image Default](./example/outputNameDefault.png)
+
+![Image](./example/outputName.png)
--- example/index.js
+++ example/index.js
@@ -1,9 +1,18 @@
 const ImageComposer = require('../index.js');
 
+const imgComposerDefault = new ImageComposer();
 const imgComposer = new ImageComposer();
+
+imgComposerDefault.compose({
+	backgroundSrc: __dirname + '/background.jpg',
+	qrcodeSrc: __dirname + '/qrcode.jpg',
+	outputPath: __dirname + '/outputNameDefault.png'
+});
+
 
 imgComposer.compose({
 	backgroundSrc: __dirname + '/background.jpg',
 	qrcodeSrc: __dirname + '/qrcode.jpg',
+	portraitSrc: __dirname + '/portrait.png',
 	outputPath: __dirname + '/outputName.png'
 });
